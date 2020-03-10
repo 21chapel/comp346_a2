b733b81bd0b8357018cdf435a1c907a5178180be
Anoop Anthony <anoop.anthony@gmail.com>
2020-03-09 21:33:49 -0400

    myBase to amadaeusBase, to see what i was missing

--- src/Comp346pa2driver.java
+++ src/Comp346pa2driver.java
@@ -1,3 +1,7 @@
+package noSynch;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -10,27 +14,30 @@
  * @author Kerly Titus
  */
 public class Comp346pa2driver {
+	public static boolean debug = false; 
+	public static boolean synched = false; 
+	public static boolean using_sem = false; 
 
-    /**
+    /** 
      * main class
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        Network objNetwork = new Network( );            /* Activate the network */
+    
+        
+    	Network objNetwork = new Network( );            /* Activate the network */
         objNetwork.start();
-
-        Server server1 = new Server("SERVER 1");                        /* Start the server */
-        server1.start();
-
-        Server server2 = new Server("SERVER 2");                        /* Start the server */
-        server2.start();
-
+        
+        Server first = new Server("first");
+        first.start();
+        Server second = new Server("second");
+        second.start();
+        
         Client objClient1 = new Client("sending");          /* Start the sending client thread */
         objClient1.start();
         Client objClient2 = new Client("receiving");        /* Start the receiving client thread */
         objClient2.start();
 
     }
-
-}
+    
+ }
